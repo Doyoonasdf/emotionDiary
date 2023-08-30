5e87d7e7c67ed2380c2fccaee53abd9044e3e112
Doyoonasdf <won1hana@naver.com>
2023-08-30 13:05:50 +0900

    기본 세팅1(폰트세팅, 버튼컴포넌트세팅,이미지 에셋 세팅)

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import New from './pages/New';
 import Edit from './pages/Edit';
 import Diary from './pages/Diary';
 
+//components
+import MyButton from './components/MyButton';
 function App() {
 	return (
 		<BrowserRouter>
@@ -15,6 +17,10 @@ function App() {
 				<img src={process.env.PUBLIC_URL + `/assets/emotion3.png`} />
 				<img src={process.env.PUBLIC_URL + `/assets/emotion4.png`} />
 				<img src={process.env.PUBLIC_URL + `/assets/emotion5.png`} /> */}
+				<MyButton text={'버튼'} onClick={() => alert('버튼클릭')} type={'positive'} />
+				<MyButton text={'버튼'} onClick={() => alert('버튼클릭')} />
+				<MyButton text={'버튼'} onClick={() => alert('버튼클릭')} type={'negative'} />
+				<MyButton text={'버튼'} onClick={() => alert('버튼클릭')} type={'asdfsdafsd'} />
 
 				<Routes>
 					<Route path='/' element={<Home />} />
