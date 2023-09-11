576eb53cea76d6c62eaf1d4ee623800112b27f30
Doyoonasdf <won1hana@naver.com>
2023-09-11 22:26:14 +0900

    수정하면 수정된 data가 2개씩 생기는 문제 해결

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -60,7 +60,7 @@ const DiaryEditor = ({ isEdit, originData }) => {
 				onEdit(originData.id, date, content, emotion);
 			}
 		}
-		onCreate(date, content, emotion);
+
 		navigate('/', { replace: true });
 	};
 
